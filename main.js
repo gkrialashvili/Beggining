a1ba33abbc2017dd8fa4681c4eb5d6024637a24d
gkrialashvili <you@example.com>
2021-04-13 13:20:00 +0400

    vue small

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 const app = Vue.createApp({
-  template: "<h1>Hello World</h1>",
+  data() {
+    return {
+      firstName: "Gio",
+      lastName: "Doe",
+      gender: "male",
+      email: "Doe@gmail.com",
+      picture: "https://randomuser.me/api/portraits/men/1.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      const user = results[0];
+
+      console.log(user);
+
+      (this.firstName = user.name.first),
+        (this.lastName = user.name.last),
+        (this.gender = user["gender"]),
+        (this.email = user.email),
+        (this.picture = user.picture.large);
+    },
+  },
+  mounted() {
+    this.getUser();
+  },
 });
+
+app.mount("#app");
